19e0abe2911e8ef17662227857b9dcd3f3908edb
Rui Yan <angruiyan@gmail.com>
2023-03-16 23:56:03 +0800

    Removed session

--- index.js
+++ index.js
@@ -1,20 +1,9 @@
 const express = require('express');
-const session = require('express-session');
 
 const { getPosts } = require('./reddit_utils.js');
-var { SESSION_SECRET } = require('./config/reddit_api.js');
 
 const app = express();
 const port = 3000;
-
-// Set up session middleware
-app.use(
-	session({
-		secret: SESSION_SECRET,
-		resave: false,
-		saveUninitialized: true,
-	})
-);
 
 app.get('/reddit/', async (req, res) => {
 	let response = await getPosts(req, res);
