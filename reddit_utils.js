b629229cb0fd70a262c8c41528246dd28b90ac45
Rui Yan <angruiyan@gmail.com>
2023-03-16 22:42:21 +0800

    Updated getPostsWithKeyword

--- reddit_utils.js
+++ reddit_utils.js
@@ -24,7 +24,13 @@ const getPostsWithKeyword = async (req, res) => {
 		const response = await axios.get(
 			`https://api.pushshift.io/reddit/search/comment?q=${req.params.question}&size=500&filter=body,score,author,permalink,created_utc`
 		);
-		return response.data['data'];
+		let data = response.data['data'];
+		for (let i = 0; i < data.length; i++) {
+			data[i]['body'] = preprocessing(data[i]['body']);
+			const date = new Date(data[i]['created_utc'] * 1000);
+			data[i]['created_utc'] = date.toLocaleString();
+		}
+		return data;
 	} catch (error) {
 		console.error(error);
 		res.status(500).send('An error occurred have token');
