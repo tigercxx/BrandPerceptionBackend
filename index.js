0c399a892e940537179c997a4b3c016ed126883c
Rui Yan <angruiyan@gmail.com>
2023-03-17 16:56:09 +0800

    Added a predict function to test

--- index.js
+++ index.js
@@ -5,10 +5,29 @@ const { getPosts } = require('./utils/reddit_utils.js');
 const app = express();
 const port = 3000;
 
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/reddit/', async (req, res) => {
 	let response = await getPosts(req, res);
 	res.send(response);
 });
+
+// Predict a single sentence
+app.post('/predict', async (req, res) => {
+	try {
+		const inputText = await req.body.inputText;
+		res.json({
+			test: inputText,
+		});
+	} catch (error) {
+		console.error(error);
+		res.status(500).send('Internal Server Error');
+	}
+});
+
+// Predict a text file
+
+// Predict an array of sentence
 
 app.listen(port, () => {
 	console.log(`Server listening at ${port}`);
