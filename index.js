4d55826e478bb3ef4045d5f074577f37051cf226
tigercxx <76241201+tigercxx@users.noreply.github.com>
2023-03-18 02:02:08 +0800

    changed port to 4242 :p

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-const express = require('express');
+const express = require("express");
 
-const { getPosts } = require('./utils/reddit_utils.js');
+const { getPosts } = require("./utils/reddit_utils.js");
 
 const app = express();
-const port = 3000;
+const port = 4242;
 
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/reddit/', async (req, res) => {
-	let response = await getPosts(req, res);
-	res.send(response);
+app.get("/reddit/", async (req, res) => {
+    let response = await getPosts(req, res);
+    res.send(response);
 });
 
 // Predict a single sentence
-app.post('/predict', async (req, res) => {
-	try {
-		const inputText = await req.body.inputText;
-		res.json({
-			test: inputText,
-		});
-	} catch (error) {
-		console.error(error);
-		res.status(500).send('Internal Server Error');
-	}
+app.post("/predict", async (req, res) => {
+    try {
+        const inputText = await req.body.inputText;
+        res.json({
+            test: inputText,
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Internal Server Error");
+    }
 });
 
 // Predict a text file
@@ -30,5 +30,5 @@ app.post('/predict', async (req, res) => {
 // Predict an array of sentence
 
 app.listen(port, () => {
-	console.log(`Server listening at ${port}`);
+    console.log(`Server listening at ${port}`);
 });
