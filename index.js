b1969fec9bcb004747b765332e5258d502bdba2e
Rui Yan <angruiyan@gmail.com>
2023-03-18 10:28:25 +0800

    Added windows bat support and changed exec to execPromise

--- index.js
+++ index.js
@@ -1,76 +1,88 @@
-const express = require("express");
+const express = require('express');
+var os = require('os');
+const fs = require('fs');
+const util = require('util');
 
-const { getPosts } = require("./utils/reddit_utils.js");
-const fs = require("fs");
-const util = require("util");
-const exec = util.promisify(require("child_process").exec);
+const execPromise = util.promisify(require('child_process').exec);
+
+const { getPosts } = require('./utils/reddit_utils.js');
 
 const app = express();
 const port = 4242;
 
+// to process json in express
 app.use((req, res, next) => {
-    if (req.originalUrl.includes("/webhook")) {
-        next();
-    } else {
-        express.json()(req, res, next);
-    }
+	if (req.originalUrl.includes('/webhook')) {
+		next();
+	} else {
+		express.json()(req, res, next);
+	}
 });
+
+// to process headers from url
 app.use(express.urlencoded({ extended: true }));
+
+// enable CORS
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    next();
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+	next();
 });
 
-app.get("/reddit/", async (req, res) => {
-    let response = await getPosts(req, res);
-    res.send(response);
+app.get('/reddit/', async (req, res) => {
+	let response = await getPosts(req, res);
+	res.send(response);
 });
 
-async function runpython() {
-    try {
-        const { stdout, stderr } = await exec(
-            "(cd ../closedAI/e2e; source work_sentence.sh)"
-        );
-        console.log("stdout:", stdout);
-        console.log("stderr:", stderr);
-    } catch (err) {
-        console.error(err);
-    }
-}
+// To run the Python Script, we will run locally in the current machine
+// There needs to be other repo that is working with this project
+// To run the script, we will call .bat (windows) or .sh (max/linux)
+const runPythonScript = async () => {
+	try {
+		let stdout, stderr;
+		if (os.platform() === 'win32') {
+			({ stdout, stderr } = await execPromise('cd ../closedAI/e2e & call work_sentence.bat'));
+		} else {
+			({ stdout, stderr } = await execPromise(
+				'(cd ../closedAI/e2e; source work_sentence.sh)'
+			));
+		}
+		console.log('stdout:', stdout);
+		console.log('stderr:', stderr);
+	} catch (err) {
+		console.error(err);
+	}
+};
 
 // Predict a single sentence
-app.post("/predict", async (req, res) => {
-    result = null;
-    try {
-        const inputText = await req.body.inputText;
-        console.log(inputText);
-        // writes text to file. will always replace text. needs to have data and sentence dir
-        fs.writeFile("../data/sentence/test.txt", inputText, (err) => {
-            if (err) {
-                throw err;
-            }
-        });
-        console.log("Data has been written to file successfully.");
-        await runpython();
-        console.log("Output written to output.json");
+app.post('/predict', async (req, res) => {
+	result = null;
+	try {
+		const inputText = await req.body.inputText;
+		console.log(inputText);
+		// writes text to file. will always replace text. needs to have data and sentence dir
+		fs.writeFile('../data/sentence/test.txt', inputText, (err) => {
+			if (err) {
+				console.log(error);
+				return;
+			}
+			console.log('Data has been written to file successfully.');
+		});
+		await runPythonScript();
 
-        fs.readFile("../data/output/output.json", "utf8", (error, data) => {
-            if (error) {
-                console.log(error);
-                return;
-            }
-            // console.log(JSON.parse(data));
-            result = JSON.parse(data);
-            res.json(result);
-        });
-    } catch (error) {
-        console.error(error);
-        res.status(500).send("Internal Server Error");
-    }
+		fs.readFile('../data/output/output.json', 'utf8', (error, data) => {
+			if (error) {
+				console.log(error);
+				return;
+			}
+			console.log('Output written to output.json');
+			result = JSON.parse(data);
+			res.json(result);
+		});
+	} catch (error) {
+		console.error(error);
+		res.status(500).send('Internal Server Error');
+	}
 });
 
 // Predict a text file
@@ -78,5 +90,5 @@ app.post("/predict", async (req, res) => {
 // Predict an array of sentence
 
 app.listen(port, () => {
-    console.log(`Server listening at ${port}`);
+	console.log(`Server listening at ${port}`);
 });
